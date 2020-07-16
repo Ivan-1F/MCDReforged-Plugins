4d1050f8df3268451ab8c4095342fa7d11a55fc5
Yifan Wang <wyf0762@foxmail.com>
2020-07-16 16:21:34 +0800

    [tick] rewrite

--- tick/tick.py
+++ tick/tick.py
@@ -1,10 +1,33 @@
+Prefix = "!!tick"
+format_error = f"§c格式错误！使用§6{Prefix}查看帮助！"
+help_msg = '''========MCDR-Tick========
+§6!!tick h§r 使用/tick health
+§6!!tick e§r 使用/tick entities'''
+
 def on_info(server, info):
     content = info.content
-    if content == "!!tick h" or content == "!!tick health":
+    cmd = content.split()
+    if cmd[0] != Prefix:
+        return
+    del cmd[0]
+
+    if len(cmd) == 0:
+        server.reply(info, help_msg)
+        return
+
+    if len(cmd) != 1:
+        server.reply(info, format_error)
+        return
+
+    if cmd[0] == "h" or cmd[0] == "health":
         server.execute("carpet commandTick true")
         server.execute("tick health")
         server.execute("carpet commandTick false")
-    elif content == "!!tick e" or content == "!!tick entities":
+
+    elif cmd[0] == "e" or cmd[0] == "entities":
         server.execute("carpet commandTick true")
         server.execute("tick entities")
         server.execute("carpet commandTick false")
+
+def on_load(server,module):
+    server.add_help_message(Prefix, "一个用于报修机器故障的插件")
